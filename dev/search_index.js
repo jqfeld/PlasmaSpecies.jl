59c957486096974bee5ec89a487c21414dd3573f
Documenter.jl <documenter@juliadocs.github.io>
2023-05-27 17:57:24 +0000

    build based on 6a5a7f4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlasmaSpecies","category":"page"},{"location":"#PlasmaSpecies","page":"Home","title":"PlasmaSpecies","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlasmaSpecies.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlasmaSpecies]","category":"page"}]
+}
